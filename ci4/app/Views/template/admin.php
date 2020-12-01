9c1f600c4917a98d4fb33557d26ee1ffcfccdb6f
Jaihan Abidin <51147214+Jeabede@users.noreply.github.com>
2020-12-01 10:35:21 +0700

    Restoran CI-4

---
+++ ci4/app/Views/template/admin.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('/bootstrap/css/bootstrap.min.css') ?>">
+    <title>Admin Page</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row mt-2">
+            <div class="col">
+                <nav class="navbar navbar-light bg-light">
+                    <a class="navbar-brand" href="<?= base_url('/admin') ?>">Dashboard</a>
+
+                    <span>User:</span>
+                    <a href="#">
+                        <div>
+                            <?php
+                                if (!empty(session()->get('user'))) {
+                                    echo session()->get('user');
+                                }
+                            ?>
+                        </div>
+                    </a>
+
+                    <span>Email:</span>
+                    <div>
+                        <?php
+                            if (!empty(session()->get('email'))) {
+                                echo session()->get('email');
+                            }
+                        ?>
+                    </div>
+                    
+                    <span>Level:</span>
+                    <div>
+                        <?php
+                            if (!empty(session()->get('level'))) {
+                                echo session()->get('level');
+                                $level = session()->get('level');
+                            }
+                        ?>
+                    </div>
+
+                    <a href="<?= base_url('admin/login/logout') ?>">Logout</a>
+
+                </nav>
+            </div>
+        </div>
+        <div class="row mt-2">
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <ul class="list-group list-group-flush">
+                        <?php if ($level === "Admin"): ?>
+                        <li class="list-group-item"><a href="<?= base_url('/admin/kategori') ?>">Kategori</a></li>
+                        <li class="list-group-item"><a href="<?= base_url('/admin/menu') ?>">Menu</a></li>
+                        <li class="list-group-item"><a href="<?= base_url('/admin/pelanggan') ?>">Pelanggan</a></li>
+                        <li class="list-group-item"><a href="<?= base_url('/admin/order') ?>">Order</a></li>
+                        <li class="list-group-item"><a href="<?= base_url('/admin/orderdetail') ?>">Order Detail</a></li>
+                        <li class="list-group-item"><a href="<?= base_url('/admin/user') ?>">User</a></li>
+                        <?php endif; ?>
+
+                        <?php if ($level === "Kasir"): ?>
+                        <li class="list-group-item"><a href="<?= base_url('/admin/order') ?>">Order</a></li>
+                        <li class="list-group-item"><a href="<?= base_url('/admin/orderdetail') ?>">Order Detail</a></li>
+                        <?php endif; ?>
+
+                        <?php if ($level === "Koki"): ?>
+                        <li class="list-group-item"><a href="<?= base_url('/admin/orderdetail') ?>">Order Detail</a></li>
+                        <?php endif; ?>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-8 px-2">
+                <?= $this->renderSection('content') ?>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ ci4/app/Views/template/shop_service.php
@@ -0,0 +1,43 @@
+<section class="shop-services section home">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-6 col-12">
+					<!-- Start Single Service -->
+					<div class="single-service">
+						<i class="ti-rocket"></i>
+						<h4>Gratis Ongkir</h4>
+						<p>Di seluruh Indonesia</p>
+					</div>
+					<!-- End Single Service -->
+				</div>
+				<div class="col-lg-3 col-md-6 col-12">
+					<!-- Start Single Service -->
+					<div class="single-service">
+						<i class="ti-reload"></i>
+						<h4>Pengiriman Cepat</h4>
+						<p>Pengiriman cepat & Higenis</p>
+					</div>
+					<!-- End Single Service -->
+				</div>
+				<div class="col-lg-3 col-md-6 col-12">
+					<!-- Start Single Service -->
+					<div class="single-service">
+						<i class="ti-lock"></i>
+						<h4>Pembayaran Aman</h4>
+						<p>100% pembayaran aman</p>
+					</div>
+					<!-- End Single Service -->
+				</div>
+				<div class="col-lg-3 col-md-6 col-12">
+					<!-- Start Single Service -->
+					<div class="single-service">
+						<i class="ti-tag"></i>
+						<h4>Harga Terbaik</h4>
+						<p>Dijamin Murah</p>
+					</div>
+					<!-- End Single Service -->
+				</div>
+			</div>
+		</div>
+	</section>
+<?= $this->renderSection('shopservice') ?>
